46a7599a3d6f81337b5c1cd9f32dfea078d340de
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-05-08 16:39:16 +0600

    some updated About & Banner section

--- legal-service-client/src/Components/AboutUs/About.jsx
+++ legal-service-client/src/Components/AboutUs/About.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-3/4 mx-auto my-40'>
-      <div className="flex justify-between items-center">
-       <div className='w-1/2'>
+    <div className='md:w-3/4 w-full mx-auto my-40'>
+      <div className="flex md:flex-row flex-col-reverse md:justify-between justify-center items-center">
+       <div className='md:w-1/2 w-full md:mt-0 mt-20 md:px-0 px-20'>
        <img src="https://img.freepik.com/free-photo/photorealistic-lawyer-environment_23-2151152267.jpg?t=st=1714042964~exp=1714046564~hmac=c26478797dc21468fc66e830081fa1f7cd1bf5013f87df164768446ffebf4f96&w=360" alt="" />
        </div>
-        <div className='w-3/4 mx-auto px-10'>
+        <div className='md:w-3/4 w-full mx-auto px-10'>
             <p className='mb-10'>About Us</p>
             <h2 className='text-5xl mb-5 font-serif'>Gives Your Mind a New Way of Thinking</h2>
             <em className='my-5 text-slate-600 text-lg'>“Notaries connect you with people all around the globe.”</em>
